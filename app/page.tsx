cb10d6d4724b5030f94c5c48e39132c68ab41dd3
akshayrx <hello.arxports@gmail.com>
2025-02-23 01:04:22 +0530

    starter kit ready

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ export default function Page() {
       <p className="mb-4">
         {`I work on AI chatbot development, SaaS solutions, and MVP development, helping startups and businesses turn ideas into real, working products. Using React.js, Next.js, and automation tools, I build simple, user-friendly solutions that solve real problems.`}
       </p>
+      <p className='mb-4'>
+        {`I'm a full-stack developer with a focus on front-end development. I have experience working with React.js, Next.js, and Node.js, and I'm always looking to learn new technologies and improve my skills.`}
+      </p>
       <div className="my-8">
         <BlogPosts />
       </div>
